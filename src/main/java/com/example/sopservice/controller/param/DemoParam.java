be86dc327e881e3b4773fbd8dd0c6b11c3f1b006
lhbings <2815834905@qq.com>
2020-09-01 11:45:50 +0800

    dev 提交

--- src/main/java/com/example/sopservice/controller/param/DemoParam.java
+++ src/main/java/com/example/sopservice/controller/param/DemoParam.java
@@ -4,7 +4,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.NotEmpty;
-/*dev change*/
+/*dev change  v2*/
 @Data
 public class DemoParam {
 
