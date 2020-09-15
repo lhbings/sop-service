dceb0ca663db8cdfd4ce1a80784132753ff87e1e
lhbings <2815834905@qq.com>
2020-09-15 18:10:37 +0800

    新增redis代码

--- src/test/java/com/example/sopservice/SopServiceApplicationTests.java
+++ src/test/java/com/example/sopservice/SopServiceApplicationTests.java
@@ -3,9 +3,12 @@ package com.example.sopservice;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.sopservice.controller.result.ErpGoodsBrandResult;
 import com.example.sopservice.entity.ErpGoodsBrand;
+import com.example.sopservice.service.TestMqService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,6 +29,24 @@ class SopServiceApplicationTests {
 		System.out.println(rel.getRecords());
 		System.out.println("-=--------------");
 		System.out.println(rel2.getRecords());
+	}
+
+	@Autowired
+	private TestMqService testMqService;
+
+	@Test
+	void testMq(){
+		testMqService.send();
+	}
+
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Test
+	void testRedis() {
+//		redisTemplate.opsForValue().set("hello", "world");
+		System.out.println(redisTemplate.opsForValue().get("hello"));
 	}
 
 }
